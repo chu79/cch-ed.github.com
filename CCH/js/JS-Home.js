0c168c7c883cbacbc95c03477de35d26551b717d
chu79 <95512972+chu79@users.noreply.github.com>
2023-08-23 14:22:48 +0800

    Ver.2

--- CCH/js/JS-Home.js
+++ CCH/js/JS-Home.js
@@ -8,6 +8,12 @@ $(document).ready(function () {
       let toggle = document.getElementById('toggle-menu');
       let sider = document.getElementById('sider')
       
+      //  toggle burger menu ---->
+      toggle.onclick = function(){
+        toggle.classList.toggle('menu-is-active');
+        sider.classList.toggle('active')
+      }
+      
       document.onclick = function(e){
         if(e.target.id !== 'sider' && e.target.id !== 'toggle-menu'){
             toggle.classList.remove('menu-is-active')
@@ -15,21 +21,54 @@ $(document).ready(function () {
         }
       }
       
-      toggle.onclick = function(){
-        toggle.classList.toggle('menu-is-active');
-        sider.classList.toggle('active')
-      }
-  
+      
       $(window).scroll(function () {
         
         var lastScrollTop = 0;
         var scrollTop = $(this).scrollTop();
-  
+        
         if (scrollTop > lastScrollTop) {
           toggle.classList.remove('menu-is-active');
           sider.classList.remove('active')
         }
       });
-
+      
+      //  toggle burger menu <----
+      
       
   });
+
+// Slide Image
+
+var images = [
+  "IMG/homeImg_2.png",
+  "IMG/homeImg_1.png",
+  "IMG/homeImg_3.png",
+  "IMG/homeImg_4.png",
+  "IMG/homeImg_6.png"
+];
+var currentImage = 0;
+
+var slide = document.getElementById("slide");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+function showImage() {
+  slide.src = images[currentImage];
+}
+
+function prevImage() {
+  currentImage = (currentImage - 1 + images.length) % images.length;
+  showImage();
+}
+
+function nextImage() {
+  currentImage = (currentImage + 1) % images.length;
+  showImage();
+}
+
+prev.addEventListener("click", prevImage);
+next.addEventListener("click", nextImage);
+
+showImage();
+
